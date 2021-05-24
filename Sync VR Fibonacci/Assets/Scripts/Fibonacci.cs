3e059880549b7d16c27a097c67e90ddc5aeea35f
Abraham Nataniel <abraham.nataniel18@gmail.com>
2021-05-24 18:30:43 +0200

    Assignment

---
+++ Sync VR Fibonacci/Assets/Scripts/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fibonacci : MonoBehaviour
+{
+    public Text fibNr;
+    public int a;
+    public int b;
+    public int c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = 0;
+        b = 1;
+        c = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fibNr.text = c.ToString();
+    }
+
+    void OnMouseUp()
+    {
+        c = fibonacciIncrement();
+    }
+
+    //Function to create the sequence
+    public int fibonacciIncrement()
+    {
+        int prev = a; //Store the current number to prev number
+        a = b; //a becomes the next number
+        b = b + prev; //Increments the next number
+
+        return a;
+    }
+}
---
+++ Sync VR Fibonacci/ReadMe.txt
@@ -0,0 +1,10 @@
+Instruction - How to play:
+- Just click on the button.
+
+Design Choices:
+Since Fibonacci is an Italian Mathematician that exists during the renaissance period, I'd figure that the
+aesthethics should follow as well.
+Hence:
+- Background: Italian Renaissance painting
+- Background music: Antonio Vivaldi - Summer, to give a melancholic vibe
+- Texts are in Italian.
